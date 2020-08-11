2d09ffda19d8d7d5aa44c415b197c2ebc49bccd0
AlmaSZ <49698648+sotoamish@users.noreply.github.com>
2020-08-11 16:38:01 -0500

    Add files via upload
    Ya van textos e imágenes en la página, incluso links, en nucleoone_data.js, para hacer la lista de reproducción, por favor.

---
+++ nucleoone_data.js
@@ -0,0 +1,17 @@
+const primera_data = {
+    1 : { 
+        'url' : 'https://www.youtube.com/embed/GDoXywDWc-s',
+    },
+    2 : {
+        'url' : 'https://www.youtube.com/embed/Cys2dYCHazk',
+    },
+    3 : {
+         'url' : 'https://www.youtube.com/embed/wGudjIOJD0M',
+    }, 
+    4 : {
+         'url' : 'https://www.youtube.com/embed/MsQWTcOs8sU',
+    },    
+    5 : {
+        'url' : 'https://www.youtube.com/embed/MsQWTcOs8sU',
+   },   
+};
