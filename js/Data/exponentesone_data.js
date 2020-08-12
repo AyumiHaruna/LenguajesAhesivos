4c52bec8d3e97fc3c7c190fbc93b9deb011f3d1d
AlmaSZ <49698648+sotoamish@users.noreply.github.com>
2020-08-12 02:16:02 -0500

    Add files via upload

---
+++ js/Data/exponentesone_data.js
@@ -0,0 +1,26 @@
+const exponentesone_data = {
+    /* Acamonchi  */
+    1 : { 
+        'url' : 'https://www.youtube.com/embed/_m11M2Otd9o',
+    },
+    /* Charritos  */
+    2 : {
+        'url' : 'https://www.youtube.com/embed/SYPR2CIWQ2E',
+    },
+    /* REM  */
+    3 : {
+         'url' : 'https://www.youtube.com/embed/ZGlG3GkJJuU',
+    }, 
+    /* Sok  */
+    4 : {
+         'url' : 'https://www.youtube.com/embed/9d7zjGbWS9w',
+    },
+    /* Ztep  */
+    5 : {
+        'url' : 'https://www.youtube.com/embed/F3xnOS_rLLA',
+    },
+    /* Twelve  */
+    6 : {
+        'url' : 'https://www.youtube.com/embed/GvESMSIQRso',
+   },   
+};
---
+++ js/exponentes.js
@@ -0,0 +1,75 @@
+//for all pages when page is loaded create navbar
+document.addEventListener("DOMContentLoaded", function() {
+    // function fron this script
+    printNavBar(); 
+});
+
+//change the video source 
+function changeVideo( key ) {
+    //get video div
+    let videoTag = document.getElementById("exponentes_video_player");
+    //get current url
+    let currentUrl = videoTag.src;
+    //get max widt of the screen
+    let vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+
+     
+        var data_origin = exponentesone_data;
+        
+        //if current url is diferent to new, change to new url
+    if( data_origin[key]['url'] != currentUrl) {
+        //set new src on the player
+        videoTag.src = data_origin[key]['url'];
+        
+        //autoscroll to video div
+        if (vw <= 992) {
+            videoBlock[0].scrollIntoView({ block: "start", behavior: "smooth" });
+        }
+    }
+    
+   collapseOthers ( key );
+}
+
+//collapse the non-selected divs
+function collapseOthers(key){
+
+    accordionList = Array.prototype.slice.call(
+        document.getElementsByClassName("participante")
+    );
+
+    accordionList.forEach(elm => {
+        if( parseInt(key) !== parseInt(elm.dataset.sesion) ) {
+            if( elm.classList.contains("show") ){
+                elm.classList.remove("show");
+            }
+        }
+    });
+}
+
+//toggle navBtns and their infoPanels
+function changeActiveButton( elm ) {
+    navBtns = Array.prototype.slice.call(
+        document.getElementsByClassName("navBtns")
+    );
+    arrInfo = Array.prototype.slice.call(
+        document.getElementsByClassName("actInfo")
+    );
+
+    objective = elm.dataset.target;
+    objective = objective.replace('#','');
+
+    navBtns.forEach(btn => {
+        if (btn !== elm) {
+            if(btn.classList.contains("active")) {
+                btn.classList.remove("active");
+            }        
+        }
+    });
+    arrInfo.forEach(col => {
+        if( col.id !== objective) {
+            col.classList.remove("show");
+        } 
+    });
+
+    elm.classList.toggle("active");
+}
