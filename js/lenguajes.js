ceb56beb553aad1af05db05932a25ddf50dd8f48
Ayumi Haruna <ayumi.harun@gmail.com>
2020-08-19 20:45:09 -0500

    inicio de nuevo branch, separando nucleos

--- js/lenguajes.js
+++ js/lenguajes.js
@@ -1,64 +1,87 @@
 document.addEventListener('DOMContentLoaded', function() {
-    //create gallery divs
-    //gallery divs must have display block when are created
-    var mySwipertest = new Swiper('.gallery_container', {
-        // Optional parameters
-        direction: 'horizontal',
-        loop: true,
-        slidesPerView: 5,
-        centeredSlides: true,
-      
-        // Navigation arrows
-        navigation: {
-          nextEl: '.gal_car_next',
-          prevEl: '.gal_car_prev',
-        },
-
-        autoplay: {
-            delay: 2000,
-            disableOnInteraction: false,
-        },
-
-        // breakpoints: {
-        //     640: {
-        //       slidesPerView: 1.5,
-        //       spaceBetween: 20,
-        //     },
-        //     768: {
-        //       slidesPerView: 2,
-        //       spaceBetween: 40,
-        //     },
-        //     1024: {
-        //       slidesPerView: 3.5,
-        //       spaceBetween: 50,
-        //     },
-        // }
-    });
-
-    var mySecondSwiper = new Swiper('.gallery_container_second', {
-        // Optional parameters
-        direction: 'horizontal',
-        loop: true,
-        slidesPerView: 5,
-        centeredSlides: true,
     
-        // Navigation arrows
-        navigation: {
-        nextEl: '.gal_sec_car_next',
-        prevEl: '.gal_sec_car_prev',
-        },
+    // index.html functions
+    var index = document.getElementsByClassName("home-div");
+    if (index != undefined) {
+        // add Jelly animation on mouse over
+        var jellyList = Array.prototype.slice.call(
+            document.getElementsByClassName("pathImg")
+        );
+        jellyList.forEach(button => {
+            button.addEventListener("mouseenter", e => {
+                if( !button.classList.contains("jello-horizontal") ){
+                    button.classList.toggle("jello-horizontal");
+                }
+            });
+            button.addEventListener("mouseleave", e => {
+                if( button.classList.contains("jello-horizontal") ){
+                    button.classList.toggle("jello-horizontal");
+                }
+            });
+        });
+    }
+    
 
-        autoplay: {
-            delay: 2000,
-            disableOnInteraction: false,
-        },
-    });
+    // nucleo1.html functions
+    var nucleo1 = document.getElementsByClassName("comun1-1");
+    if (nucleo1 != undefined ){
+        // create both galleries
+        var mySwipertest = new Swiper('.gallery_container', {
+            // Optional parameters
+            direction: 'horizontal',
+            loop: true,
+            slidesPerView: 5,
+            centeredSlides: true,
+        
+            // Navigation arrows
+            navigation: {
+            nextEl: '.gal_car_next',
+            prevEl: '.gal_car_prev',
+            },
 
-    //time to hide galery divs
-    document.getElementById("comun1-1").style.display = "none"; 
-    document.getElementById("comun1-2").style.display = "none"; 
-    // document.getElementById("comun1-3").style.display = "none"; 
+            autoplay: {
+                delay: 2000,
+                disableOnInteraction: false,
+            },
 
+            // breakpoints: {
+            //     640: {
+            //       slidesPerView: 1.5,
+            //       spaceBetween: 20,
+            //     },
+            //     768: {
+            //       slidesPerView: 2,
+            //       spaceBetween: 40,
+            //     },
+            //     1024: {
+            //       slidesPerView: 3.5,
+            //       spaceBetween: 50,
+            //     },
+            // }
+        });
+
+        var mySecondSwiper = new Swiper('.gallery_container_second', {
+            // Optional parameters
+            direction: 'horizontal',
+            loop: true,
+            slidesPerView: 5,
+            centeredSlides: true,
+        
+            // Navigation arrows
+            navigation: {
+            nextEl: '.gal_sec_car_next',
+            prevEl: '.gal_sec_car_prev',
+            },
+
+            autoplay: {
+                delay: 2000,
+                disableOnInteraction: false,
+            },
+        });
+
+        //hide second gallery
+        document.getElementById("comun1-2").style.display = "none"; 
+    }
 }, false);
 
 
@@ -144,4 +167,12 @@ function changeTo( current, next ){
             elmEnter.classList.remove(animation);
         }, 300);     
     }
+}
+
+
+//muestra pop-up con imagen seleccionada
+function showImage( sel ){
+    console.log(sel);
+    let popUp = document.getElementById("picSelection");
+    popUp.style.display = "block";
 }
