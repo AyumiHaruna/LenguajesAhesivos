4ca1b99a87c1bb7ace3852b7f497e9f4cc578990
AlmaSZ <49698648+sotoamish@users.noreply.github.com>
2020-08-21 14:57:04 -0500

    2da lista de reproducción de videos de exponentes
    por favor, para el reproductor de núcleo 2

---
+++ js/Data/exponentestwo_data.js
@@ -0,0 +1,30 @@
+const exponentestwo_data = {
+    /* KETS  */
+    1 : { 
+        'url' : 'https://www.youtube.com/embed/-TMvRQm4L68',
+    },
+    /* MEDINA (Street Chickens)  */
+    2 : {
+        'url' : 'https://www.youtube.com/embed/_rNhNC5Zx2I',
+    },
+    /* Tierra y Tinta  */
+    3 : {
+         'url' : 'https://www.youtube.com/embed/eACnrMl4ZVU',
+    }, 
+    /*  REIGT (T!GR3) */
+    4 : {
+         'url' : 'https://www.youtube.com/embed/JywK-MgqJAU',
+    },
+    /*  EKINOXIIA */
+    5 : {
+        'url' : 'https://www.youtube.com/embed/2QuHaoTIUH8',
+    },
+    /*  WOLF & GREEN    */
+    6 : {
+        'url' : 'https://www.youtube.com/embed/aNAJJGYTGQg',
+   },  
+     /*  SETO */
+    7 : {
+        'url' : 'https://www.youtube.com/embed/p-uFKXqYfI4',
+   },   
+};
